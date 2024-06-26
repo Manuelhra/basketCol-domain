ea1a18a46597f5ff18e633a4e3da71f89d1b7b34
manuelh_ra <manuelhra.dev@gmail.com>
2024-06-26 10:14:36 -0500

    LeagueSEASONnAME CREATED

--- src/unitedHoops/leagueSeason/domain/value-objects/LeagueSeasonName.ts
+++ src/unitedHoops/leagueSeason/domain/value-objects/LeagueSeasonName.ts
@@ -1,7 +1,25 @@
+import PropertyLengthExceededError from '../../../shared/domain/exceptions/PropertyLengthExceededError';
+import PropertyLengthTooShortError from '../../../shared/domain/exceptions/PropertyLengthTooShortError';
 import StringValueObject from '../../../shared/domain/value-objects/StringValueObject';
 
 class LeagueSeasonName extends StringValueObject {
   readonly #LENGTH: { min: number; max: number } = { min: 8, max: 20 } as const;
+
+  constructor(value: string) {
+    super(value, 'name');
+
+    this.ensureIsValidName(value, 'name');
+  }
+
+  private ensureIsValidName(value: string, propertyName: string): void {
+    if (value.length < this.#LENGTH.min) {
+      throw new PropertyLengthTooShortError(propertyName, this.#LENGTH.min, value.length);
+    }
+
+    if (value.length > this.#LENGTH.max) {
+      throw new PropertyLengthExceededError(propertyName, this.#LENGTH.max, value.length);
+    }
+  }
 }
 
 export default LeagueSeasonName;
