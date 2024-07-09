ff947e2cc66f6faa6b1a17c95e6ff4b191a5218a
manuelh_ra <manuelhra.dev@gmail.com>
2024-07-09 16:01:26 -0500

    LeagueValidationService created

---
+++ src/unitedHoops/league/domain/services/LeagueValidationService.ts
@@ -0,0 +1,25 @@
+import { Nullable } from '../../../shared/domain/Nullable';
+import LeagueNotFoundError from '../exceptions/LeagueNotFoundError';
+import League from '../League';
+import { LeagueRepository } from '../repository/LeagueRepository';
+import LeagueId from '../value-objects/LeagueId';
+
+class LeagueValidationService {
+  readonly #leagueRepository: LeagueRepository;
+
+  constructor(dependencies: {
+    leagueRepository: LeagueRepository,
+  }) {
+    this.#leagueRepository = dependencies.leagueRepository;
+  }
+
+  public async ensureLeagueExist(leagueId: LeagueId): Promise<void> {
+    const leagueFound: Nullable<League> = await this.#leagueRepository.searchById(leagueId);
+
+    if (leagueFound === undefined || leagueFound === null) {
+      throw new LeagueNotFoundError(leagueId);
+    }
+  }
+}
+
+export default LeagueValidationService;
