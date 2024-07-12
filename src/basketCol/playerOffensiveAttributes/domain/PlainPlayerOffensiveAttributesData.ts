a91fbe396eebb06f600a3c938bb98c671a79878b
manuelh_ra <manuelhra.dev@gmail.com>
2024-07-11 20:09:51 -0500

    offensive attributes and phisycal attributes created

---
+++ src/basketCol/playerOffensiveAttributes/domain/PlainPlayerOffensiveAttributesData.ts
@@ -0,0 +1,8 @@
+export interface PlainPlayerOffensiveAttributesData {
+  id: string;
+  closeShot: number;
+  drivingLayup: number;
+  drivingDunk: number;
+  standingDunk: number;
+  postControl: number;
+}
---
+++ src/basketCol/playerPhysicalAttributes/domain/PlainPlayerPhysicalAttributesData.ts
@@ -0,0 +1,8 @@
+export interface PlainPlayerPhysicalAttributesData {
+  id: string;
+  speed: number;
+  acceleration: number;
+  strength: number;
+  vertical: number;
+  stamina: number;
+}
