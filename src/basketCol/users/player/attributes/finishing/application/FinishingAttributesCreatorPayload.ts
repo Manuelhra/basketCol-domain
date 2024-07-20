052c74713ca690a0aca48e6176663441c203f77b
manuelh_ra <manuelhra.dev@gmail.com>
2024-07-20 12:10:42 -0500

    FinishingAttributes flow success

---
+++ src/basketCol/users/player/attributes/finishing/application/FinishingAttributesCreatorPayload.ts
@@ -0,0 +1,8 @@
+export interface FinishingAttributesCreatorPayload {
+  id: string;
+  drivingLayup: number;
+  drivingDunk: number;
+  standingDunk: number;
+  postControl: number;
+  playerUserId: string;
+}
---
+++ src/basketCol/users/player/attributes/finishing/domain/repository/FinishingAttributesRepository.ts
@@ -0,0 +1,5 @@
+import FinishingAttributes from '../FinishingAttributes';
+
+export interface FinishingAttributesRepository {
+  save(finishingAttributes: FinishingAttributes): Promise<void>;
+}
