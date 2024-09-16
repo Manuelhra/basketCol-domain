cc5f9d290e80536244e2a45fe5943ee8d9cf8993
manuelh_ra <manuelhra.dev@gmail.com>
2024-09-16 11:58:14 -0500

    Add new methods on UserAccountState

--- src/basketCol/users/shared/domain/value-objects/UserAccountState.ts
+++ src/basketCol/users/shared/domain/value-objects/UserAccountState.ts
@@ -9,6 +9,42 @@ export abstract class UserAccountState extends StringValueObject {
     this.ensureIsValidAccountState(value);
   }
 
+  static active(): string {
+    return this.#VALID_STATES['0'];
+  }
+
+  static inactive(): string {
+    return this.#VALID_STATES['1'];
+  }
+
+  static locked(): string {
+    return this.#VALID_STATES['2'];
+  }
+
+  static suspended(): string {
+    return this.#VALID_STATES['3'];
+  }
+
+  static banned(): string {
+    return this.#VALID_STATES['4'];
+  }
+
+  static underReview(): string {
+    return this.#VALID_STATES['5'];
+  }
+
+  static deleted(): string {
+    return this.#VALID_STATES['6'];
+  }
+
+  static archived(): string {
+    return this.#VALID_STATES['7'];
+  }
+
+  static getValidStates(): readonly string[] {
+    return UserAccountState.#VALID_STATES;
+  }
+
   private ensureIsValidAccountState(value: string): void {
     if (!UserAccountState.#VALID_STATES.includes(value as any)) {
       throw new InvalidEnumValueError('accountState', value, UserAccountState.#VALID_STATES);
