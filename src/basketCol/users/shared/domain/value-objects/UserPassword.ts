8b6956f7b5e77cdbefd9009e978f0b152b00176c
manuelh_ra <manuelhra.dev@gmail.com>
2024-09-20 12:46:10 -0500

    Implement new method

--- src/basketCol/users/shared/domain/value-objects/UserPassword.ts
+++ src/basketCol/users/shared/domain/value-objects/UserPassword.ts
@@ -25,6 +25,10 @@ export class UserPassword extends StringValueObject {
     return UserPassword.#REQUIREMENTS;
   }
 
+  public static getRequirementsAsString(): string {
+    return UserPassword.#REQUIREMENTS.join(', ');
+  }
+
   private static ensureIsValidPassword(password: string): void {
     if (!UserPassword.#PASSWORD_REG_EXP.test(password)) {
       throw new PasswordPolicyViolationError(`The password does not allow the value <${password}>: ${UserPassword.#REQUIREMENTS.join(', ')}`);
