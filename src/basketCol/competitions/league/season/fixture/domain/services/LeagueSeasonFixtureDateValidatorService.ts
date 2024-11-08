a6777cb271e1456d8fe54df87d3e3a3e038d20d3
manuelh_ra <manuelhra.dev@gmail.com>
2024-11-08 12:31:19 -0500

    Fix service

---
+++ src/basketCol/competitions/league/season/fixture/domain/services/LeagueSeasonFixtureDateValidatorService.ts
@@ -0,0 +1,40 @@
+import { Nullable } from '../../../../../../shared/domain/Nullable';
+import { DateNotWithinLeagueSeasonError } from '../../../../../shared/domain/exceptions/DateNotWithinLeagueSeasonError';
+import { LeagueSeasonNotFoundError } from '../../../domain/exceptions/LeagueSeasonNotFoundError';
+import { LeagueSeason } from '../../../domain/LeagueSeason';
+import { ILeagueSeasonRepository } from '../../../domain/repository/ILeagueSeasonRepository';
+import { LSFixtureDate } from '../value-objects/LSFixtureDate';
+import { LSFixtureLeagueSeasonId } from '../value-objects/LSFixtureLeagueSeasonId';
+
+type Dependencies = {
+  leagueSeasonRepository: ILeagueSeasonRepository;
+};
+
+export class LeagueSeasonFixtureDateValidatorService {
+  readonly #leagueSeasonRepository: ILeagueSeasonRepository;
+
+  private constructor(dependencies: Dependencies) {
+    this.#leagueSeasonRepository = dependencies.leagueSeasonRepository;
+  }
+
+  public static create(dependencies: Dependencies): LeagueSeasonFixtureDateValidatorService {
+    return new LeagueSeasonFixtureDateValidatorService(dependencies);
+  }
+
+  public async ensureDateWithinLeagueSeason(
+    leagueSeasonId: LSFixtureLeagueSeasonId,
+    date: LSFixtureDate,
+  ): Promise<void> {
+    const leagueSeasonFound: Nullable<LeagueSeason> = await this.#leagueSeasonRepository.searchById(leagueSeasonId.value);
+
+    if (leagueSeasonFound === null || leagueSeasonFound === undefined) {
+      throw LeagueSeasonNotFoundError.create(leagueSeasonId.value);
+    }
+
+    const { startDate, endDate } = leagueSeasonFound.toPrimitives;
+
+    if (date.dateAsString < startDate || date.dateAsString > endDate) {
+      throw DateNotWithinLeagueSeasonError.create(leagueSeasonId.value, date.dateAsString);
+    }
+  }
+}
