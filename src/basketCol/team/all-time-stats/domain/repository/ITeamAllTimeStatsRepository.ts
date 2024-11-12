23623b6a904f743d97ba8bc5c2b287f35cef8d26
manuelh_ra <manuelhra.dev@gmail.com>
2024-11-11 19:00:23 -0500

    Agregar método faltante

--- src/basketCol/team/all-time-stats/domain/repository/ITeamAllTimeStatsRepository.ts
+++ src/basketCol/team/all-time-stats/domain/repository/ITeamAllTimeStatsRepository.ts
@@ -1,5 +1,8 @@
+import { Nullable } from '../../../../shared/domain/Nullable';
 import { TeamAllTimeStats } from '../TeamAllTimeStats';
+import { TATStatsId } from '../value-objects/TATStatsId';
 
 export interface ITeamAllTimeStatsRepository {
   save(teamAllTimeStats: TeamAllTimeStats): Promise<void>;
+  findById(teamAllTimeStatsId: TATStatsId): Promise<Nullable<TeamAllTimeStats>>;
 }
