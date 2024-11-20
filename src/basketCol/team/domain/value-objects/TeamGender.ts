9f921d57f02399bc2a4f4f4e13bd8a79c77271a9
manuelh_ra <manuelhra.dev@gmail.com>
2024-11-20 18:35:31 -0500

    Expose getter

--- src/basketCol/team/domain/value-objects/TeamGender.ts
+++ src/basketCol/team/domain/value-objects/TeamGender.ts
@@ -1,11 +1,26 @@
+import { InvalidGenderError } from '../../../shared/domain/exceptions/InvalidGenderError';
 import { GenderValueObject } from '../../../shared/domain/value-objects/GenderValueObject';
 
 export class TeamGender extends GenderValueObject {
+  static readonly #VALID_GENDERS: string[] = ['FEMALE', 'MALE', 'MIXED'] as const;
+
   private constructor(value: string) {
     super(value);
+
+    TeamGender.#ensureIsValidGender(value);
   }
 
   static create(value: string): TeamGender {
     return new TeamGender(value);
+  }
+
+  public static get validGenders(): string[] {
+    return TeamGender.#VALID_GENDERS;
+  }
+
+  static #ensureIsValidGender(gender: string): void {
+    if (!TeamGender.#VALID_GENDERS.includes(gender)) {
+      throw InvalidGenderError.create(gender, TeamGender.#VALID_GENDERS);
+    }
   }
 }
