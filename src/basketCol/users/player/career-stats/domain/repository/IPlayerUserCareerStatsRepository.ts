049bda9c5cd76313446a434a1a5ead17afc4d0b9
manuelh_ra <manuelhra.dev@gmail.com>
2024-11-22 14:58:37 -0500

    Implementar nuevo método en el repositorio

--- src/basketCol/users/player/career-stats/domain/repository/IPlayerUserCareerStatsRepository.ts
+++ src/basketCol/users/player/career-stats/domain/repository/IPlayerUserCareerStatsRepository.ts
@@ -1,8 +1,10 @@
 import { Nullable } from '../../../../../shared/domain/Nullable';
 import { PlayerUserCareerStats } from '../PlayerUserCareerStats';
 import { PUCStatsId } from '../value-objects/PUCStatsId';
+import { PUCStatsPlayerUserId } from '../value-objects/PUCStatsPlayerUserId';
 
 export interface IPlayerUserCareerStatsRepository {
   save(playerUserCareerStats: PlayerUserCareerStats): Promise<void>;
   findById(pUCStatsId: PUCStatsId): Promise<Nullable<PlayerUserCareerStats>>;
+  findByPlayerUserId(playerUserId: PUCStatsPlayerUserId): Promise<Nullable<PlayerUserCareerStats>>;
 }
