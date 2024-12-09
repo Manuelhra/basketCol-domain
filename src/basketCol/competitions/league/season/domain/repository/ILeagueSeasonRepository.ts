7b3f17c7f81b1c7c58c9dfcabfc2ccc828c77b28
manuelh_ra <manuelhra.dev@gmail.com>
2024-12-08 20:30:44 -0500

    Implementar nuevo método

--- src/basketCol/competitions/league/season/domain/repository/ILeagueSeasonRepository.ts
+++ src/basketCol/competitions/league/season/domain/repository/ILeagueSeasonRepository.ts
@@ -1,10 +1,12 @@
 import { IPaginatedResponse } from '../../../../../shared/domain/IPaginatedResponse';
 import { Nullable } from '../../../../../shared/domain/Nullable';
 import { LeagueSeason } from '../LeagueSeason';
+import { LeagueSeasonLeagueId } from '../value-objects';
 import { LeagueSeasonId } from '../value-objects/LeagueSeasonId';
 
 export interface ILeagueSeasonRepository {
   save(leagueSeason: LeagueSeason): Promise<void>;
   findById(leagueSeasonId: LeagueSeasonId): Promise<Nullable<LeagueSeason>>;
+  findAllByLeagueId(leagueId: LeagueSeasonLeagueId): Promise<LeagueSeason[]>;
   searchAll(params: { query?: string; page: number; perPage: number; }): Promise<IPaginatedResponse<LeagueSeason>>;
 }
