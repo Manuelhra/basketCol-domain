5e398f01ac3d6bea4214a76396e7fd2a73ba13bf
manuelh_ra <manuelhra.dev@gmail.com>
2024-12-09 12:02:02 -0500

    Crear método findById

--- src/basketCol/competitions/league/season/awards/domain/repository/ILeagueSeasonAwardsRepository.ts
+++ src/basketCol/competitions/league/season/awards/domain/repository/ILeagueSeasonAwardsRepository.ts
@@ -1,8 +1,10 @@
 import { Nullable } from '../../../../../../shared/domain/Nullable';
 import { LeagueSeasonAwards } from '../LeagueSeasonAwards';
+import { LeagueSeasonAwardsId } from '../value-objects/LeagueSeasonAwardsId';
 import { LSALeagueSeasonId } from '../value-objects/LSALeagueSeasonId';
 
 export interface ILeagueSeasonAwardsRepository {
+  findById: (id: LeagueSeasonAwardsId) => Promise<Nullable<LeagueSeasonAwards>>;
   findByLeagueSeasonId(leagueSeasonId: LSALeagueSeasonId): Promise<Nullable<LeagueSeasonAwards>>;
   save(leagueSeasonAwards: LeagueSeasonAwards): Promise<void>;
 }
