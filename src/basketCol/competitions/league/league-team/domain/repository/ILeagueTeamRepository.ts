1039b86d67d0d54e6be8d59bc509d5dbe54f4b94
manuelh_ra <manuelhra.dev@gmail.com>
2024-12-08 21:08:30 -0500

    Implementar metodo

--- src/basketCol/competitions/league/league-team/domain/repository/ILeagueTeamRepository.ts
+++ src/basketCol/competitions/league/league-team/domain/repository/ILeagueTeamRepository.ts
@@ -1,6 +1,7 @@
 import { IPaginatedResponse } from '../../../../../shared/domain/IPaginatedResponse';
 import { Nullable } from '../../../../../shared/domain/Nullable';
 import { LeagueTeam } from '../LeagueTeam';
+import { LeagueTeamStatus } from '../value-objects';
 import { LeagueTeamId } from '../value-objects/LeagueTeamId';
 import { LeagueTeamLeagueId } from '../value-objects/LeagueTeamLeagueId';
 import { LeagueTeamTeamId } from '../value-objects/LeagueTeamTeamId';
@@ -9,5 +10,6 @@ export interface ILeagueTeamRepository {
   save(leagueTeam: LeagueTeam): Promise<void>;
   findById(leagueTeamId: LeagueTeamId): Promise<Nullable<LeagueTeam>>;
   findByLeagueIdAndTeamId(leagueId: LeagueTeamLeagueId, teamId: LeagueTeamTeamId): Promise<Nullable<LeagueTeam>>;
+  findAllByLeagueId(leagueId: LeagueTeamLeagueId, status: LeagueTeamStatus): Promise<LeagueTeam[]>;
   searchAll(params: { query?: string; page: number; perPage: number; }): Promise<IPaginatedResponse<LeagueTeam>>;
 }
