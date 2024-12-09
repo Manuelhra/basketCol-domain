abafbf6b175ba3f55b558222c39f4768bb9884d2
manuelh_ra <manuelhra.dev@gmail.com>
2024-12-09 10:47:34 -0500

    Exportar nuevo método en el repositoro

--- src/basketCol/competitions/league/season/awards/domain/repository/ILeagueSeasonAwardsRepository.ts
+++ src/basketCol/competitions/league/season/awards/domain/repository/ILeagueSeasonAwardsRepository.ts
@@ -1,5 +1,8 @@
+import { Nullable } from '../../../../../../shared/domain/Nullable';
 import { LeagueSeasonAwards } from '../LeagueSeasonAwards';
+import { LSALeagueSeasonId } from '../value-objects/LSALeagueSeasonId';
 
 export interface ILeagueSeasonAwardsRepository {
+  findByLeagueSeasonId(leagueSeasonId: LSALeagueSeasonId): Promise<Nullable<LeagueSeasonAwards>>;
   save(leagueSeasonAwards: LeagueSeasonAwards): Promise<void>;
 }
