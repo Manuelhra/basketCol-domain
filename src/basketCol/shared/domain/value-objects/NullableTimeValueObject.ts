db0738431da4ccfac6d19823e9084d0844667a48
manuelh_ra <manuelhra.dev@gmail.com>
2024-12-10 18:24:06 -0500

    Fix method

--- src/basketCol/shared/domain/value-objects/NullableTimeValueObject.ts
+++ src/basketCol/shared/domain/value-objects/NullableTimeValueObject.ts
@@ -3,7 +3,7 @@ import { ValueObject } from './ValueObject';
 
 export abstract class NullableTimeValueObject extends ValueObject<string | null> {
   protected constructor(value: string | null, propertyName: string) {
-    super(value, propertyName, 'string | null', { allowUndefined: true });
+    super(value, propertyName, 'string | null', { allowNull: true });
 
     if (value !== null) {
       TimeValueObject.ensureIsValidTime(value);
