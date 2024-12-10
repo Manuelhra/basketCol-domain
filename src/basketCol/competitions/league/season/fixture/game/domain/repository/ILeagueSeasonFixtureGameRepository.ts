978f1977bcb0c0f21f303be7e5c630cbb7af9aff
manuelh_ra <manuelhra.dev@gmail.com>
2024-12-09 21:53:37 -0500

    Implementar nuevo método

--- src/basketCol/competitions/league/season/fixture/game/domain/repository/ILeagueSeasonFixtureGameRepository.ts
+++ src/basketCol/competitions/league/season/fixture/game/domain/repository/ILeagueSeasonFixtureGameRepository.ts
@@ -1,8 +1,10 @@
 import { Nullable } from '../../../../../../../shared/domain/Nullable';
+import { FixtureId } from '../../../../../../shared/domain/value-objects/FixtureId';
 import { LeagueSeasonFixtureGame } from '../LeagueSeasonFixtureGame';
 import { LSFGameId } from '../value-objects/LSFGameId';
 
 export interface ILeagueSeasonFixtureGameRepository {
   save(leagueSeasonFixtureGame: LeagueSeasonFixtureGame): Promise<void>;
   findById(leagueSeasonFixtureGameId: LSFGameId): Promise<Nullable<LeagueSeasonFixtureGame>>;
+  findAllByFixtureId(fixtureId: FixtureId): Promise<LeagueSeasonFixtureGame[]>;
 }
