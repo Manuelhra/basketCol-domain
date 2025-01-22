9912807b16ac494a327ec60208261cebe689518d
manuelh_ra <manuelhra.dev@gmail.com>
2025-01-22 12:57:41 -0500

    Crear métodos necesarios para actualizar

--- src/basketCol/users/player/career-stats/domain/repository/IPlayerUserCareerStatsRepository.ts
+++ src/basketCol/users/player/career-stats/domain/repository/IPlayerUserCareerStatsRepository.ts
@@ -5,6 +5,7 @@ import { PUCStatsPlayerUserId } from '../value-objects/PUCStatsPlayerUserId';
 
 export interface IPlayerUserCareerStatsRepository {
   save(playerUserCareerStats: PlayerUserCareerStats): Promise<void>;
+  update(playerUserCareerStats: PlayerUserCareerStats): Promise<void>;
   findById(pUCStatsId: PUCStatsId): Promise<Nullable<PlayerUserCareerStats>>;
   findByPlayerUserId(playerUserId: PUCStatsPlayerUserId): Promise<Nullable<PlayerUserCareerStats>>;
 }
