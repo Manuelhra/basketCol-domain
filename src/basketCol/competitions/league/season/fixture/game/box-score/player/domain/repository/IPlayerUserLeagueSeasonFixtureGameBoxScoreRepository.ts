634227b39c8b13537904e183d53b0b01d3567817
manuelh_ra <manuelhra.dev@gmail.com>
2025-02-01 21:20:13 -0500

    aGREGAR MÉTODOS FALTANTES en domain repostory box

--- src/basketCol/competitions/league/season/fixture/game/box-score/player/domain/repository/IPlayerUserLeagueSeasonFixtureGameBoxScoreRepository.ts
+++ src/basketCol/competitions/league/season/fixture/game/box-score/player/domain/repository/IPlayerUserLeagueSeasonFixtureGameBoxScoreRepository.ts
@@ -1,5 +1,8 @@
+import { Nullable } from '../../../../../../../../../shared/domain/Nullable';
 import { PlayerUserLeagueSeasonFixtureGameBoxScore } from '../PlayerUserLeagueSeasonFixtureGameBoxScore';
+import { PLSFGBoxScoreFixtureGameId } from '../value-objects/PLSFGBoxScoreFixtureGameId';
 
 export interface IPlayerUserLeagueSeasonFixtureGameBoxScoreRepository {
   save(playerUserLeagueSeasonFixtureGameBoxScore: PlayerUserLeagueSeasonFixtureGameBoxScore): Promise<void>;
+  findById(pLSFGBoxScoreFixtureGameId: PLSFGBoxScoreFixtureGameId): Promise<Nullable<PlayerUserLeagueSeasonFixtureGameBoxScore>>;
 }
--- src/basketCol/competitions/league/season/fixture/game/box-score/team/domain/repository/ITeamLeagueSeasonFixtureGameBoxScoreRepository.ts
+++ src/basketCol/competitions/league/season/fixture/game/box-score/team/domain/repository/ITeamLeagueSeasonFixtureGameBoxScoreRepository.ts
@@ -1,5 +1,8 @@
+import { Nullable } from '../../../../../../../../../shared/domain/Nullable';
 import { TeamLeagueSeasonFixtureGameBoxScore } from '../TeamLeagueSeasonFixtureGameBoxScore';
+import { TLSFGBoxScoreFixtureGameId } from '../value-objects/TLSFGBoxScoreFixtureGameId';
 
 export interface ITeamLeagueSeasonFixtureGameBoxScoreRepository {
   save(teamLeagueSeasonFixtureGameBoxScore: TeamLeagueSeasonFixtureGameBoxScore): Promise<void>;
+  findById(tLSFGBoxScoreFixtureGameId: TLSFGBoxScoreFixtureGameId): Promise<Nullable<TeamLeagueSeasonFixtureGameBoxScore>>;
 }
